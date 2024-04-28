89dbb5e64b41e8cea10a949b09a64a3af968fe8c
wasikhan <wasikhan7099@gmail.com>
2024-04-28 17:42:38 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// var mainDate = new Date()
+//  var date = mainDate.getFullYear(2023);
+
+
+
+
+// console.log(mainDate)
+// const person = {
+//     name: "wasi" ,//property
+//     greeting(){
+//         console.log('hey i am wasi');
+//     },
+// };
+// // console.log(person);
+// console.log(person.name);
+// person.greeting();
+
+function fullName(firstName, lastName){
+var name = `${firstName} ${lastName}`; 
+var result = name.toLocaleUpperCase();
+console.log(result)
+
+}
+fullName('samm','jhon')
