7768e84851f3078ad3543ff279ff45f4336f70d6
wasikhan <wasikhan7099@gmail.com>
2024-05-12 20:14:52 +0500

    powerfull array

--- app.js
+++ app.js
@@ -15,10 +15,104 @@
 // console.log(person.name);
 // person.greeting();
 
-function fullName(firstName, lastName){
-var name = `${firstName} ${lastName}`; 
-var result = name.toLocaleUpperCase();
-console.log(result)
 
+
+// function fullName(firstName, lastName){
+// var name = `${firstName} ${lastName}`; 
+// var result = name.toLocaleUpperCase();
+// console.log(result)
+
+// }
+// fullName('samm','jhon')
+
+// spread opertor
+
+// var a = {
+//     name :'wasikhan',
+//     fName: "fahim",
+
+// }
+// var b ={...a, fName : 'khan', no : 31222 , name : ''}
+// console.log(a)
+// console.log(b)
+
+// termaly opertor
+
+// var a = 2 < 1;
+// var b;
+// a ? b =5 : b = 6 
+// console.log(b)
+// console.log(b)
+//  const foo=(a) =>{
+//     console.log( 'hello world')
+//  }
+//  foo(1)
+
+
+// higher oder call back
+
+
+
+const morning = () => {
+    console.log(`good morning  ${name}`)
 }
-fullName('samm','jhon')
+
+
+// arrya 99
+
+const students = [
+    {
+        id: 1,
+        name: 'wasi khan',
+        subject: 'chemistry',
+        score: 98,
+    },
+    {
+        id: 2,
+        name: 'taha khan',
+        subject: 'math',
+        score: 32,
+    },
+    {
+        id: 3,
+        name: 'fahim Khan',
+        subject: 'physics',
+        score: 77,
+    },
+    {
+        id: 4,
+        name: 'bob',
+        subject: 'biology',
+        score: 87,
+    },
+    {
+        id: 5,
+        name: 'peter',
+        subject: 'urdu',
+        score: 22,
+    }
+
+]
+const updatedStudents =  students.map((item) => {
+    return {
+        ...item,
+          role:"Studnet"
+    }
+})
+console.log(updatedStudents)
+
+
+// HighScore
+// const highscore = students.filter((item) => {
+//     return item.score >= 80
+// })
+// console.log(highscore)
+
+//specifed
+
+
+const speified = students.find((iid)=>{
+
+    return iid.id = 5
+})
+console.log(speified)
