e766269a824be386aa2e8d8246af9b6d7579abac
Wasi khan <154990809+Wasikhan54@users.noreply.github.com>
2024-05-19 04:05:54 +0500

    reduce assigment

--- app.js
+++ app.js
@@ -1,118 +1,105 @@
-// var mainDate = new Date()
-//  var date = mainDate.getFullYear(2023);
-
-
-
-
-// console.log(mainDate)
-// const person = {
-//     name: "wasi" ,//property
-//     greeting(){
-//         console.log('hey i am wasi');
+// const students = [
+//     {
+//         id: 1,
+//         name: 'wasi khan',
+//         subject: 'chemistry',
+//         score: 98,
 //     },
-// };
-// // console.log(person);
-// console.log(person.name);
-// person.greeting();
+//     {
+//         id: 2,
+//         name: 'taha khan',
+//         subject: 'math',
+//         score: 32,
+//     },
+//     {
+//         id: 3,
+//         name: 'fahim Khan',
+//         subject: 'math',
+//         score: 77,
+//     },
+//     {
+//         id: 4,
+//         name: 'bob',
+//         subject: 'urdu',
+//         score: 87,
+//     },
+//     {
+//         id: 5,
+//         name: 'peter',
+//         subject: 'urdu',
+//         score: 22,
+//     }
+// ]
+// // const aveargeScore = students.reduce((int , curr) => {
+// //         return int + curr.score
+// //     },0)
+// //     const totalAveargeScoe = aveargeScore/5
+// //     console.log(totalAveargeScoe)
 
 
 
-// function fullName(firstName, lastName){
-// var name = `${firstName} ${lastName}`; 
-// var result = name.toLocaleUpperCase();
-// console.log(result)
 
-// }
-// fullName('samm','jhon')
-
-// spread opertor
-
-// var a = {
-//     name :'wasikhan',
-//     fName: "fahim",
-
-// }
-// var b ={...a, fName : 'khan', no : 31222 , name : ''}
-// console.log(a)
-// console.log(b)
-
-// termaly opertor
-
-// var a = 2 < 1;
-// var b;
-// a ? b =5 : b = 6 
-// console.log(b)
-// console.log(b)
-//  const foo=(a) =>{
-//     console.log( 'hello world')
-//  }
-//  foo(1)
-
-
-// higher oder call back
-
-
-
-const morning = () => {
-    console.log(`good morning  ${name}`)
-}
-
-
-// arrya 99
-
+// servey
 const students = [
-    {
-        id: 1,
-        name: 'wasi khan',
-        subject: 'chemistry',
-        score: 98,
-    },
-    {
-        id: 2,
-        name: 'taha khan',
-        subject: 'math',
-        score: 32,
-    },
-    {
-        id: 3,
-        name: 'fahim Khan',
-        subject: 'physics',
-        score: 77,
-    },
-    {
-        id: 4,
-        name: 'bob',
-        subject: 'biology',
-        score: 87,
-    },
-    {
-        id: 5,
-        name: 'peter',
-        subject: 'urdu',
-        score: 22,
+    { id: 1, name: 'wasi khan', subject: 'chemistry', score: 98 },
+    { id: 2, name: 'taha khan', subject: 'math', score: 32 },
+    { id: 3, name: 'fahim Khan', subject: 'math', score: 77 },
+    { id: 4, name: 'bob', subject: 'urdu', score: 87 },
+    { id: 5, name: 'peter', subject: 'urdu', score: 22 }
+];
+
+const subjectCounts = students.reduce((acc, student) => {
+    const { subject } = student;
+    if (acc[subject]) {
+        acc[subject]++;
+    } else {
+        acc[subject] = 1;
     }
+    return acc;
+}, {});
 
-]
-const updatedStudents =  students.map((item) => {
-    return {
-        ...item,
-          role:"Studnet"
-    }
-})
-console.log(updatedStudents)
+console.log(subjectCounts);
 
 
-// HighScore
-// const highscore = students.filter((item) => {
-//     return item.score >= 80
-// })
-// console.log(highscore)
-
-//specifed
 
 
-const speified = students.find((iid)=>{
 
-    return iid.id = 5
-})
-console.log(speified)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// toug
+
+// const students = [
+//     { id: 1, name: 'wasi khan', subject: 'chemistry', score:89 },
+//     { id: 2, name: 'taha khan', subject: 'math', score: 89 },
+//     { id: 3, name: 'fahim Khan', subject: 'math', score: 77 },
+//     { id: 4, name: 'bob', subject: 'urdu', score: 87 },
+//     { id: 5, name: 'peter', subject: 'urdu', score: 22 }
+// ];
+// const arrr=[1,3,4,56,65 ,9,67,2,8]
+// const subjectCounts = arrr.reduce((acc, student) => {
+//     // const { subject } = student;
+//     // if (acc[score]) {
+//     //     acc[score]++;
+//     // } else {
+//     //     acc[subject] = 1;
+//     // }
+//     return acc + student
+// },0);
+
+// console.log(subjectCounts);
